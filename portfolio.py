d77a76493e0c8ae509645f92dcf2b1dfcdb8902d
James H. <lsaucyfx@gmail.com>
2024-02-17 20:09:44 -0500

    updated portfolio class

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,7 @@
 # PORTFOLIO CLASS
-
+import pandas as pd 
+import numpy as np 
+import datetime as dt
 class Portfolio:
     def __init__(self):
         self.positions = {}
@@ -8,6 +10,20 @@ class Portfolio:
 
         # <TODO> implement me
 
+
+    def generate_ema_signal(self, close_price, window):
+        ema = close_price.ewm(span=window, adjust=False).mean()
+        # 1 -> buy, 0 -> sell
+        signals = np.where(close_price > ema, 1, 0)
+
+
+
+    def buy(self, date, price):
+
+
+    def sell(self, date, price):
+
+    
     def calculate_performance(self):
 
         # <TODO> implement me
