015c603d84d73527402b31ec9a584889b08d57e7
James H. <lsaucyfx@gmail.com>
2024-02-17 19:38:13 -0500

    added portfolio class

---
+++ portfolio.py
@@ -0,0 +1,16 @@
+# PORTFOLIO CLASS
+
+class Portfolio:
+    def __init__(self):
+        self.positions = {}
+
+    def execute_trades(self.signals, historical_data):
+
+        # <TODO> implement me
+
+    def calculate_performance(self):
+
+        # <TODO> implement me
+
+        # Calculate portfolio performance metrics
+        # print/return perf results
