f1f194a1b22c94f2e6086470a4d2ef79345f85a5
James H. <lsaucyfx@gmail.com>
2024-02-17 19:40:43 -0500

    added DataSource class

---
+++ data_source.py
@@ -0,0 +1,13 @@
+# DATA SOURCE CLASS
+
+import pandas as pd
+
+
+class DataSource:
+    def fetch_data(self):
+
+        # <TODO> implement me:
+        # Fetch historical XAU/USD DATA 
+        # RETURN DATA IN DATAFRAME FORMAT
+
+
