e1658a05f3b9f4e500a0931e63d4e397061c83a6
James H. <lsaucyfx@gmail.com>
2024-02-17 19:49:55 -0500

    added XAU/USD Historical Data

--- data_source.py
+++ data_source.py
@@ -4,10 +4,15 @@ import pandas as pd
 
 
 class DataSource:
+    
     def fetch_data(self):
-
+        
+        
         # <TODO> implement me:
-        # Fetch historical XAU/USD DATA 
+        # Fetch historical XAU/USD DATA
+        file = ('./XAUUSD_052021_022024.csv')
+        df = pd.read_csv(file)
+        # later make it automatic
         # RETURN DATA IN DATAFRAME FORMAT
 
 
