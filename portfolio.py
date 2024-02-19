d3a25554d6b45f84d41090bf5974187c401e92ba
James H. <lsaucyfx@gmail.com>
2024-02-18 19:13:48 -0500

    removed old execute trade method

--- portfolio.py
+++ portfolio.py
@@ -1,10 +1,6 @@
 # PORTFOLIO CLASS
-from typing import List
-import pandas as pd
 import numpy as np
-import datetime as dt
 
-from pandas.core.indexes.datetimes import DatetimeTimedeltaMixin
 from strategy import Strategy
 
 
@@ -13,19 +9,6 @@ class Portfolio:
         self.positions = {}
         self.cash_balance = initial_balance
         self.transaction_history = []
-
-    def execute_trades(self, close_prices):
-
-        # <TODO> implement me
-        signals = Strategy.generate_ema_signals(close_prices, window=50)
-
-        for i in range(1, len(signals)):
-            if signals[i] == 1 and signals[i - 1] == 0:
-                # buy signals
-                self.buy(historical_data.index[i], historical_data["Close"][i])
-            elif signals[i] == -1 and signals[i - 1] == 0:
-                # sell signal
-                self.sell(historical_data.index[i], historical_data["Close"][i])
 
     def generate_ema_signal(self, close_price, window):
         ema = close_price.ewm(span=window, adjust=False).mean()
@@ -61,7 +44,7 @@ class Portfolio:
             )
 
     def sell(self, date, price):
-    # check if there is any shares to self
+        # check if there is any shares to self
         if date not in self.positions or self.positions[date] == 0:
             print(f"No shares to sell on {date}")
             return
@@ -70,18 +53,19 @@ class Portfolio:
             shares_to_sell = self.positions[date]
 
             # update the positions and cash cash_balance
-            self.positions[date] = 0 
+            self.positions[date] = 0
             self.cash_balance += shares_to_sell * price
 
             # record the transaction in transaction_history
 
-            self.transaction_history.append({
-                'Date': date,
-                'Action': 'Sell',
-                'Price': price,
-                'Shares': shares_to_sell
-            })
-    
+            self.transaction_history.append(
+                {
+                    "Date": date,
+                    "Action": "Sell",
+                    "Price": price,
+                    "Shares": shares_to_sell,
+                }
+            )
 
     def calculate_performance(self):
 
