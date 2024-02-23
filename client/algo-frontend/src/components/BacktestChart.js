6beee2092f0b5f785b18ba3f36fab31af2ccaa66
James H. <lsaucyfx@gmail.com>
2024-02-22 19:46:35 -0500

    added Backtestchart component

---
+++ client/algo-frontend/src/components/BacktestChart.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const BacktestChart = ({ data }) => {
+	const chartData = {
+		labels: data.map(entry => entry.date),
+		datasets: [
+			{
+				label: 'Portfolio Value',
+				data: data.map(entry => entry.portfolioValue),
+				borderColor: 'rgba(75,192,192,1)',
+				borderWidth: 2,
+				fill: false,
+			},
+		],
+	};
+
+const options = {
+	scales: {
+		x: {
+			type: 'time',
+			time: {
+			  unit: 'day',
+			},
+		},
+		y: {
+			beginAtZero: true,
+		},
+	},
+};
+return <Line data={chartData} options={options}/>;
+};
+export default BacktestChart;
+
+
